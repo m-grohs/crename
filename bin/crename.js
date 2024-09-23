a16384518e7b4e401cd620f45337b3f1448b3165
mgrohs <marc.grohs@gmail.com>
2024-09-23 15:28:14 +0200

    fixed forgotten comments that disable functionality

--- bin/crename.js
+++ bin/crename.js
@@ -115,9 +115,9 @@ async function defaultRename(args) {
 						process.exit(9);
 					}
 					const newName = args[0] + chapNum[0];
-					// rename(file, newName, (err) => {
-					// 	if (err) throw err;
-					// });
+					rename(file, newName, (err) => {
+						if (err) throw err;
+					});
 					console.log(`"${file}" renamed to: "${newName}"`);
 				}
 			}
@@ -189,7 +189,7 @@ async function customRename(args) {
 				// Only rename Files with Extension to skip Folders
 				if (fileExt) {
 					const newName = baseName + baseNum + fileExt;
-					// renameSync(file, newName);
+					renameSync(file, newName);
 					console.log(`"${file}" renamed to: "${newName}"`);
 					baseNum++;
 				}
